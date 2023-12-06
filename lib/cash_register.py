c1e79eb53ba5c2f607b7d682bf55a4b8925af46a
Chris Lozzi <clozzi12@gmail.com>
2023-12-06 15:37:01 -0500

    halfway through

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,43 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount=0):
+    self.discount = discount
+    self.total = 0
+    self.items = []
+
+  @property
+  def discount(self):
+    return self._discount
+
+  @discount.setter
+  def discount(self, discount):
+    if isinstance(discount, (int, float)):
+      self._discount = discount
+    else:
+      print("not a number")
+
+  def add_item(self, title, price, quantity=1):
+    self.items.extend([(title, price)] * quantity)
+    self.total += (price * quantity)
+
+  def apply_discount(self):
+    if self._discount > 0:
+      discount_amt = self.total * (self._discount / 100)
+      self.total -= discount_amt
+      print(f"After the discount, the total comes to ${int(self.total)}.")
+    else:
+      print("There is no discount to apply.")
+
+  def get_items(self):
+      return list(self.items)
+  
+  def void_last_transaction(self):
+    if self.items:
+      last_price = self.items[-1][1]
+      self.items.pop()
+      self.total -= last_price
+    else:
+      print("No items to remove.")
+
+  
